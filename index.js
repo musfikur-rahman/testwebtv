2c625a37d594f3de0d949d35fa74d92571029113
musfikur-rahman <musfik.rizvi@gmail.com>
2022-05-20 21:45:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -23,17 +23,3 @@ if(Hls.isSupported()) {
   });
   
 }
-
-window.onresize = function () {
-  windowSize();
-}
-
-document.onreadystatechange = function () {
-  windowSize();
-}
-
-async function windowSize() {
-  document.getElementById('video').style.height = (window.innerHeight - 20) + 'px';
-  document.getElementById('video').style.width = (window.innerWidth - 20) + 'px';
-  document.getElementById('video').style.overflow = scroll;
-}
