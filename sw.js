850f1cbaf6445a8173e15132dff88d321fcc1603
musfikur-rahman <musfik.rizvi@gmail.com>
2022-05-24 20:54:26 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+const cacheName = 'v3';
+const staticAssets = [
+  'hls.js@latest',
+  'index.html',
+  'index.js',
+  'logo192.png',
+  'logo512.png',
+  'manifest.webmanifest',
+  'sw.js'
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
