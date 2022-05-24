f0f4aa1cba0f5eec56483413c230300691dda4aa
musfikur-rahman <musfik.rizvi@gmail.com>
2022-05-24 16:32:23 +0600

    Update windowsetup.js

--- windowsetup.js
+++ windowsetup.js
@@ -8,6 +8,6 @@ document.onreadystatechange = function () {
 
 async function windowSize() {
     document.getElementById('video').style.height = (window.innerHeight - 10) + 'px';
-    document.getElementById('video').style.width = (window.innerWidth) + 'px';
+    document.getElementById('video').style.width = (window.innerWidth - 10) + 'px';
     document.getElementById('video').style.overflow = scroll;
 }
