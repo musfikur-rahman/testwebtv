7cd6bd78ebcf2baf43f94435b45f7a39d80603df
musfikur-rahman <musfik.rizvi@gmail.com>
2022-05-27 20:35:00 +0600

    Update hlsplayer.js

--- hlsplayer.js
+++ hlsplayer.js
@@ -4,9 +4,10 @@ var url_3 = 'aHR0cDovLzEwMy41NS4xNDQuNDYvaGxzL3Qtc3BvcnRzLm0zdTg=';
 var url_4 = 'aHR0cHM6Ly90ZW1waW5mb2Jhc2UuamFnb2JkLmNvbTo0NDQvY1pWeWRtVnlYOFJwYkVVOU1pOHhOeTh5TURFMEdJRFU2Umd6UTZOVEFnZEVvYWVGemJGOTJZV3hJWlQwMFUwZXpOMUl6TXlmdmNHVk1aRUpDVEVGV2VWTjNQVE9tZEZzYVdSdGFXNTFhaVBoblBUSS90aXRhc2guc3RyZWFtL2NodW5rcy5tM3U4';
 var url_5 = 'aHR0cHM6Ly90ZW1waW5mb2Jhc2UuamFnb2JkLmNvbTo0NDQvY1pWeWRtVnlYOFJwYkVVOU1pOHhOeTh5TURFMEdJRFU2Umd6UTZOVEFnZEVvYWVGemJGOTJZV3hJWlQwMFUwZXpOMUl6TXlmdmNHVk1aRUpDVEVGV2VWTjNQVE9tZEZzYVdSdGFXNTFhaVBoblBUSS9hdG5iZC04LW9yZy5zdHJlYW0vY2h1bmtzLm0zdTg=';
 
-var videomodal = document.getElementById("video-modal");
+var videomodal = document.getElementById("video");
 var spanmodal = document.getElementById("span-modal");
 videomodal.style.display='none';
+spanmodal.style.display='none';
 spanmodal.className='w3-button w3-display-topright w3-red';
 
 document.getElementById("ch01").onclick = function() { hlsplayer(url_5); videomodaldisplay(); }
@@ -14,6 +15,7 @@ document.getElementById("ch02").onclick = function() { hlsplayer(url_4); videomo
 
 spanmodal.onclick = function(){
   videomodal.style.display='none';
+  spanmodal.style.display='none';
 }
 
 async function hlsplayer(urlsrc){
@@ -30,10 +32,11 @@ async function hlsplayer(urlsrc){
 
 async function videomodaldisplay(){
   videomodal.style.display='block';
+  spanmodal.style.display='block';
   videomodal.style.position='absolute';
   videomodal.style.left='0';
   videomodal.style.right='0';
   videomodal.style.top='0';
   videomodal.style.bottom='0';
-  videomodal.style.background='white';
+  videomodal.style.background='white';  
 }
