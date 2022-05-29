4699e25105e98e6b958efe14c35a410b264c85c0
musfikur-rahman <musfik.rizvi@gmail.com>
2022-05-29 16:36:57 +0600

    Update windowsetup.js

--- windowsetup.js
+++ windowsetup.js
@@ -7,7 +7,8 @@ document.onreadystatechange = function () {
 }
 
 async function windowSize() {
-  document.getElementById('video').style.height = (document.documentElement.clientHeight - 5) + 'px';
-  document.getElementById('video').style.width = (document.documentElement.clientWidth - 5) + 'px';
-  document.getElementById('video').style.overflow = scroll;
+  var videodisplay = document.getElementById("video");
+  videodisplay.style.height = (document.documentElement.clientHeight - 5) + 'px';
+  videodisplay.style.width = (document.documentElement.clientWidth - 5) + 'px';
+  videodisplay.style.overflow = scroll;
 }
